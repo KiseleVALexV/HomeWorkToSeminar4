57441fa5c259ae9e2c0583a74c24685c06e02bb2
Alexey <manoil77@list.ru>
2022-11-13 21:07:34 +0300

    Add Zadacha025

---
+++ Zadacha025_VozvestiChisloAVStepenB/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. 
+// Для решения задания использование цикла for является обязательным условием. 
+// Не использовать встроенный метод возведения в степень.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int A, int B)
+{
+    int resault = A;
+    for (int i = 1; i < B; i++)
+    {
+        resault = resault * A;
+    }
+    return resault;
+}
+
+Console.WriteLine("Введите первое число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число : ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {a} в степени {b} = {Exponentiation(a, b)}");
+
+Console.ReadKey();
