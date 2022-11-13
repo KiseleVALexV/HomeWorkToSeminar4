cdd33a5a4510a901ed3617db916299816851199a
Alexey <manoil77@list.ru>
2022-11-13 21:08:21 +0300

    Add Zadacha029

---
+++ Zadacha029_ZapolnenieMassiva/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Заполнение массива производит пользователь.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array = new int[8];
+Console.WriteLine("Введите 8 чисел : ");
+for (int i=0; i<8; i++)
+{
+    array[i] = System.Convert.ToInt32(Console.ReadLine());   
+}
+
+for (int i=0; i<8; i++)
+{
+    Console.Write($" {array[i]} ");   
+}
+
+Console.ReadKey();
